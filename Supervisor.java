c3b39275580220827b8fd2a83d650dfe16585dd4
ZafoG <zafer.elemen@studmail.w-hs.de>
2026-04-13 16:44:06 +0200

    added some fun functions :D

--- Supervisor.java
+++ Supervisor.java
@@ -24,8 +24,21 @@ public class Supervisor extends Employee{
         }
     }
 
+    public void shoutAt(Employee employee) {
+        System.out.println("WHAT ARE YOU DOING? WE GOTTA WORK!!");
+    }
+
     public void talkAbout(Employee employee) {
-        if (employee.)
+        if (employee.isWorking()) {
+            System.out.println("I am watching " + employee.getName() + " while he works.");
+        } else {
+            shoutAt(employee);
+        }
     }
     
+    public static void main(String[] args) {
+        Supervisor s1 = new Supervisor("Mehmet", 23);
+
+        s1.talkAbout(m1);
+    }
 }
